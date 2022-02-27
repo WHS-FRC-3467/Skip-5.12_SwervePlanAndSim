8935c2dc3ed38313204cbd7f33a1c47e969ada44
Scott Kukshtel <scottkukshtel@team3467.org>
2022-02-27 17:08:23 -0500

    Added drive stick filtering

--- src/main/java/frc/robot/subsystems/drive/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveDrive.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems.drive;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.DriveConstants;
 
 import java.util.function.DoubleSupplier;
 
@@ -11,6 +13,9 @@ public class SwerveDrive extends CommandBase{
     DoubleSupplier m_translationXSupplier;
     DoubleSupplier m_translationYSupplier;
     DoubleSupplier m_rotationSupplier;
+
+    SlewRateLimiter m_XRateLimiter = new SlewRateLimiter(0.5);
+    SlewRateLimiter m_YRateLimiter = new SlewRateLimiter(0.5);
 
     //Constructor for SwerveDrive
     public SwerveDrive(DriveSubsystem driveSubsystem, DoubleSupplier translationXSupplier, DoubleSupplier translationYSupplier, DoubleSupplier rotationSupplier) {
@@ -26,9 +31,9 @@ public class SwerveDrive extends CommandBase{
         // You can use `new ChassisSpeeds(...)` for robot-oriented movement instead of field-oriented movement 
         m_driveSubsystem.drive(
             ChassisSpeeds.fromFieldRelativeSpeeds(
-                m_translationXSupplier.getAsDouble(),
-                m_translationYSupplier.getAsDouble(),
-                m_rotationSupplier.getAsDouble(),
+                m_XRateLimiter.calculate(modifyAxis(m_translationXSupplier.getAsDouble())),
+                m_YRateLimiter.calculate(modifyAxis(m_translationYSupplier.getAsDouble())),
+                modifyAxis(m_rotationSupplier.getAsDouble()),
                 m_driveSubsystem.getGyroscopeRotation()
             )
         );
@@ -38,4 +43,26 @@ public class SwerveDrive extends CommandBase{
     public void end(boolean interrupted) {
         m_driveSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
     }
+
+    private static double modifyAxis(double value) {
+      
+        // Apply Deadband
+          double dband = DriveConstants.STICK_DEADBAND;
+          if (Math.abs(value) > dband) {
+            if (value > 0.0) {
+                return (value - dband) / (1.0 - dband);
+            } else {
+                return (value + dband) / (1.0 - dband);
+            }
+          } else {
+            value = 0.0;
+          }
+        
+        // Square the axis for better control range at low speeds
+        value = Math.copySign(value * value, value);
+    
+        return value;
+      }
+    
+
 }
